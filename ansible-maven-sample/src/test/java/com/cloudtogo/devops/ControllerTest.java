0c70846dad6a0f63606e4c460f10d43aac6c63da
biancl <biancl900922@163.com>
2020-03-27 08:11:30 +0800

    #SAMPLE-23 修复单元测试错误

--- ansible-maven-sample/src/test/java/com/cloudtogo/devops/ControllerTest.java
+++ ansible-maven-sample/src/test/java/com/cloudtogo/devops/ControllerTest.java
@@ -1,21 +1,24 @@
 package com.cloudtogo.devops;
 
-import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
+@WebAppConfiguration
 public class ControllerTest {
 	
 	private MockMvc mockMvc;
